074b994b67a33ecbebcfc14bfb9f9f868c206e3a
SlavikkUS <200996st@gmail.com>
2022-08-31 13:03:09 +0300

    Ch

--- SolutMir.java
+++ SolutMir.java
@@ -1,6 +1,6 @@
 public class SolutMir {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("pope");
+        System.out.println("pope");//123
     }
 }
