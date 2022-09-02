a68564e665d501112c4cdf0f011952c22bbf7956
SlavikkUS <200996st@gmail.com>
2022-09-02 11:56:24 +0300

    Home

---
+++ Main.java
@@ -0,0 +1,22 @@
+/*
+ *
+ *
+ * author Usenkov Slava;
+ */
+package Task11;
+
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        numbers(6, 8, 1);
+    }
+
+    public static void numbers(int width, int length, int radius) {
+        if (radius >= (int) Math.sqrt(length * length + width * width) / 2)
+            System.out.println("Картонка с радиусом " + radius + " закрывает полностью отвертие размера " + width + " * " + length);
+        else
+            System.out.println("Картонка с радиусом " + radius + " не закрывает полностью отвертие размера " + width + " * " + length);
+    }
+
+}
