581c0c8c2af4bc5847628a4e05e09c0b66d04ad5
SlavikkUS <200996st@gmail.com>
2024-09-23 10:47:57 +0300

    Fourth chenges

--- src/by/it_academy/practice/homework3/Homework3.java
+++ src/by/it_academy/practice/homework3/Homework3.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Homework3 {
 
     public static void main(String[] args) {
-        // findPrimeNumbers(findNumbers());
+        findPrimeNumbers(findNumbers());
         findDegreeNumbers(findNumbers());
         findSymmetryFirstAndLastNumbers();
         findRealTriangle();
