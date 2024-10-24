7c92017f650533a81af5bc2a5b366bb97414ba91
SlavikkUS <200996st@gmail.com>
2024-10-24 15:27:31 +0300

    Homework eight

--- src/by/it_academy/practice/homework8/Homework8.java
+++ src/by/it_academy/practice/homework8/Homework8.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 
 public class Homework8 {
     public static void main(String[] args) {
-        findSolutionTask1(getScanner());
-        findSolutionTask2(getScanner());
+        findSolutionTask1();
+        findSolutionTask2();
         findSolutionTask3();
         findSolutionTask4();
         findSolutionTask5();
@@ -19,56 +19,57 @@ public class Homework8 {
         return text;
     }
 
-    public static void findSolutionTask1(String txt) {
+    public static void findSolutionTask1() {
+        System.out.println("Введите строку (текст). Показать номера символов, совпадающих с последним символом строки.");
+        String txt = getScanner();
         char lastDigit = txt.charAt(txt.length() - 1);
         System.out.println("Last word is - " + lastDigit + "\n" + "Numbers of index:");
         for (int i = 0; i < txt.length(); i++) {
             char c = txt.charAt(i);
             if (c == lastDigit) {
-                System.out.print(i + " ");
+                System.out.println(i + " ");
             }
         }
     }
 
-    public static boolean findSolutionTask2(String st) {
-        int rightNumbers = st.length() - 1;
+    public static void findSolutionTask2() {
+        System.out.println("Введите число (проверка на симметричность)");
+        String number = getScanner();
+        int rightNumbers = number.length() - 1;
         int leftNumbers = 0;
         boolean flag = true;
         while (leftNumbers < rightNumbers) {
-            if (st.charAt(leftNumbers) != st.charAt(rightNumbers))
+            if (number.charAt(leftNumbers) != number.charAt(rightNumbers))
                 flag = false;
             leftNumbers++;
             rightNumbers--;
         }
-        return flag;
+        if (flag == true) {
+            System.out.println("Число является симметричным");
+        } else {
+            System.out.println("Число не является симметричным");
+        }
     }
 
     public static void findSolutionTask3() {
-        String txt = "Hello it's my first test and final second Test";
+        String text = "Hello it's my first test and i have second Test, for :test";
+        Pattern pattern = Pattern.compile("[Tt]est");
+        Matcher matcher = pattern.matcher(text);
         int count = 0;
-        String[] strings = txt.split(" ");
-        for (String s : strings) {
-            if (s.equalsIgnoreCase("test")) {
-                count++;
-            }
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+            count++;
         }
-        System.out.println("Количество вхождений слова \"test\" = " + count);
+        System.out.println(text + "\n" + "Количество вхождений слова \"test\" = " + count);
     }
 
     public static void findSolutionTask4() {
         String str = "1975 1976 1977 1978 1979 1980 1981 1982 1983 1984";
-        String firstAge = "1977";
-        String lastAge = "1982";
-        Pattern pattern = Pattern.compile(firstAge);
-        Pattern pattern2 = Pattern.compile(lastAge);
+        Pattern pattern = Pattern.compile("1977 1978 1979 1980 1981 1982");
         Matcher matcher = pattern.matcher(str);
-        Matcher matcher2 = pattern2.matcher(str);
         System.out.println("Years:");
-        while (matcher.find() && matcher2.find()) {
-            for (int i = Integer.parseInt(matcher.group()); i <= Integer.parseInt(matcher2.group()); i++) {
-                System.out.println(i);
-
-            }
+        while (matcher.find()) {
+            System.out.println(matcher.group());
         }
     }
 
