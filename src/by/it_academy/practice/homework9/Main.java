b63de7a69899b4997a3d07b27c8aacda39529e2b
SlavikkUS <200996st@gmail.com>
2024-11-02 14:58:09 +0300

    Homework eight

---
+++ src/by/it_academy/practice/homework9/Main.java
@@ -0,0 +1,21 @@
+package by.it_academy.practice.homework9;
+
+public class Main {
+    public static void main(String[] args) {
+        Shop shop = new Shop("Train");
+        Order order1 = new Order("Slava", 1);
+
+        order1.addItem(Item.BALL, Item.REFRIGERATOR);
+        Order order2 = new Order("Vika", 2);
+        order2.addItem(Item.BALL, Item.REFRIGERATOR);
+
+        Order order3 = new Order("Pete", 3);
+        order3.addItem(Item.PLAY_STATION, Item.TV, Item.BALL);
+
+        shop.addOrders(order1, order2, order3);
+        System.out.println(shop);
+        shop.removeOrder("Pete");
+        System.out.println(shop);
+    }
+}
+
---
+++ src/by/it_academy/practice/homework9/Order.java
@@ -0,0 +1,48 @@
+package by.it_academy.practice.homework9;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Order {
+    String nameBuyer;
+    int numberInLine;
+    List<Item> arrayList;
+
+    public Order(String nameBuyer, int numberInLine) {
+        this.arrayList = new ArrayList<>();
+        this.nameBuyer = nameBuyer;
+        this.numberInLine = numberInLine;
+    }
+
+    public String getNameBuyer() {
+        return nameBuyer;
+    }
+
+    public void addItem(Item... item) {
+        arrayList.addAll(Arrays.asList(item));
+    }
+
+    public void removeItem(Item item) {
+        arrayList.remove(item);
+    }
+
+    public int getSum() {
+        int sum = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            sum = sum + arrayList.get(i).getPrice();
+        }
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Order{");
+        sb.append("arrayList=").append(arrayList);
+        sb.append(", nameBuyer='").append(nameBuyer).append('\'');
+        sb.append(", numberInLine=").append(numberInLine);
+        sb.append('}');
+        return sb.toString();
+    }
+}
